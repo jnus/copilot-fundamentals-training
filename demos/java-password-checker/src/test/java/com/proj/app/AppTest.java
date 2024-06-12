27a9604ce93afd7960b8c314e532a46beacc4c93
Jasper Nygaard <jasper@sovs.net>
2024-06-12 19:45:26 +0000

    added the java password checker demo

---
+++ demos/java-password-checker/src/test/java/com/proj/app/AppTest.java
@@ -0,0 +1,18 @@
+package com.proj.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
